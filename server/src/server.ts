52245456a666d2a90d8ba8e3b51b35ac35f8fbb7
LeoDalcegio <leoodalcegio@hotmail.com>
2020-08-05 20:20:20 -0300

    Added cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,13 @@
 import express from 'express';
 import routes from './routes';
+import cors from 'cors';
 
 const PORT = process.env.PORT || 3333;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
-
-
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
