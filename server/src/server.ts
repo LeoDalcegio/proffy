32af78c146618341d85bfeb4a8f60eb8b165f141
Leonardo Luís Dalcegio <leoodalcegio@hotmail.com>
2020-08-11 12:24:49 +0000

    Added helmet.js

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import routes from './routes';
 import cors from 'cors';
+import helmet from 'helmet';
 
 const PORT = process.env.PORT || 3333;
 
@@ -8,6 +9,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(helmet());
+
 app.use(routes);
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
